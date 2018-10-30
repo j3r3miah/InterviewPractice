609da600fbecf67dff9d49bee5bdfac9992a72b5
Jeremiah Boyle <jeremiah.boyle@gmail.com>
2018-10-29 18:32:29 -0700

    BST algorithms

--- bst.py
+++ bst.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 class Node:
     data = None
     left = None
@@ -7,108 +10,272 @@ class Node:
         self.data = data
 
     def __repr__(self):
-        return str(self.data)
+        return f'Node<{self.data}>'
 
 
 class BST:
     root = None
 
-    def insert(self, data):
-        def _insert(root, new_node):
-            if not root:
-                return new_node
-            if new_node.data < root.data:
-                root.left = _insert(root.left, new_node)
-            else:
-                root.right = _insert(root.right, new_node)
-            return root
+    def __init__(self, items=None):
+        if items:
+            for o in items:
+                self.insert(o)
 
+    def insert(self, data):
+        def _insert(root, node):
+            if root is None:
+                return node
+            if node.data < root.data:
+                root.left = _insert(root.left, node)
+            elif node.data > root.data:
+                root.right = _insert(root.right, node)
+            # if node is a duplicate, it is not inserted
+            return root
         self.root = _insert(self.root, Node(data))
 
-    def bfs(self):
-        from collections import deque
-        q = deque([self.root])
-        while q:
-            node = q.popleft()
-            if node:
-                print(node.data)
-                q.append(node.left)
-                q.append(node.right)
+    def search(self, data):
+        cur = self.root
+        while cur is not None:
+            if cur.data == data:
+                return True
+            elif data < cur.data:
+                cur = cur.left
+            else:
+                cur = cur.right
+        return False
 
-    def levels(self):
+    def delete(self, data):
+        def _min_node(root):
+            while root.left:
+                root = root.left
+            return root
+
+        def _delete(root, data):
+            if root is None:
+                return root
+            if data < root.data:
+                root.left = _delete(root.left, data)
+            elif data > root.data:
+                root.right = _delete(root.right, data)
+            else: # we found the node to delete
+                # only one child, just jump over the node to delete
+                if root.left is None:
+                    return root.right
+                elif root.right is None:
+                    return root.left
+                # node to delete has two children. replace node with in-order
+                # successor and delete successor.
+                successor = _min_node(root.right)
+                root.data = successor.data
+                root.right = _delete(root.right, successor.data)
+            return root
+
+        self.root = _delete(self.root, data)
+
+    def validate(self):
+        def _validate(root):
+            if root is None:
+                return True
+            if root.left and root.left.data >= root.data:
+                return False
+            if root.right and root.right.data <= root.data:
+                return False
+            return _validate(root.left) and _validate(root.right)
+        return _validate(self.root)
+
+    def traverse_in_order(self):
         rv = []
-        found = False
-        if self.root:
-            rv.append([self.root])
-            found = True
-        while found:
-            found = False
-            next_level = []
-            for n in rv[-1]:
-                if n:
-                    next_level.append(n.left)
-                    next_level.append(n.right)
-                    if n.left or n.right:
-                        found = True
-                else:
-                    next_level.extend([None, None])
-            if found:
-                rv.append(next_level)
+        def _traverse(root):
+            if root is None:
+                return
+            _traverse(root.left)
+            rv.append(root.data)
+            _traverse(root.right)
+        _traverse(self.root)
         return rv
 
-    def __repr__(self):
-        lines = []
-        levels = self.levels()
-        max_nodes = 2**(len(levels) - 1)
-        item_width = 5
-        space = ' '
+    def traverse_level_order(self):
+        rv = []
+        level = [self.root]
+        while any(level):
+            rv.append(level)
+            level = []
+            for node in rv[-1]:
+                if node is None:
+                    continue
+                level.append(node.left)
+                level.append(node.right)
+        return [[n.data if n else None for n in level] for level in rv]
 
-        for d, level in enumerate(levels):
-            line1 = []
-            line2 = []
-            num_nodes = 2**d
-            margin_width = int(
-                (max_nodes - num_nodes + 1) * item_width / (num_nodes + 1)
+    def traverse_paths(self):
+        def _dfs(root, path, rv):
+            if root is None:
+                return
+            path.append(root.data)
+            if root.left is None and root.right is None:
+                rv.append(path[:])
+            else:
+                if root.left:
+                    _dfs(root.left, path, rv)
+                if root.right:
+                    _dfs(root.right, path, rv)
+            path.pop()
+        rv = []
+        _dfs(self.root, [], rv)
+        return rv
+
+    def has_path_sum(self, value):
+        def _has_path_sum(root, value):
+            if root is None:
+                return value == 0
+            return (
+                _has_path_sum(root.left, value - root.data) or
+                _has_path_sum(root.right, value - root.data)
             )
-            print(margin_width)
-            line1.append(space * margin_width)
-            line2.append(space * margin_width)
-            for i, n in enumerate(level):
-                if n:
-                    if i % 2 == 0:
-                        connector = '/'.rjust(item_width)
-                    else:
-                        connector = '\\'.ljust(item_width)
-                    line1.append(connector)
-                    line2.append(str(n.data).center(item_width))
-                else:
-                    line1.append(space * item_width)
-                    line2.append(space * item_width)
-                line1.append(space * margin_width)
-                line2.append(space * margin_width)
-            if d > 0:
-                lines.append(''.join(line1))
-            lines.append(''.join(line2))
-        return '\n'.join(lines)
+        return _has_path_sum(self.root, value)
+
+
+    @property
+    def minimum(self):
+        if self.root is None:
+            return None
+        cur = self.root
+        while cur.left:
+            cur = cur.left
+        return cur.data
+
+    @property
+    def maximum(self):
+        if self.root is None:
+            return None
+        cur = self.root
+        while cur.right:
+            cur = cur.right
+        return cur.data
+
+    @property
+    def depth(self):
+        def _depth(root):
+            if root is None:
+                return 0
+            return 1 + max(_depth(root.left), _depth(root.right))
+        return _depth(self.root)
+
+    def __len__(self):
+        def _count(root):
+            if root is None:
+                return 0
+            return 1 + _count(root.left) + _count(root.right)
+        return _count(self.root)
+
+    def __eq__(self, other):
+        if self is other:
+            return True
+        def _equals(root1, root2):
+            if root1 is None or root2 is None:
+                return root1 == root2
+            if root1.data != root2.data:
+                return False
+            return (
+                _equals(root1.left, root2.left) and
+                _equals(root1.right, root2.right)
+            )
+        return _equals(self.root, other.root)
+
+
+class TestBST:
+    def test_insert(self):
+        tree = BST([5, 9, 7, 3, 2])
+        assert(tree.root.data == 5)
+        assert(tree.root.right.data == 9)
+        assert(tree.root.right.left.data == 7)
+        assert(tree.root.left.data == 3)
+        assert(tree.root.left.left.data == 2)
+
+    def test_search(self):
+        items = [5, 9, 7, 3, 2]
+        tree = BST(items)
+        for o in items:
+            assert(tree.search(o))
+            assert(tree.search(o * 10) == False)
+
+    def test_delete(self):
+        tree = BST([5, 9, 7, 3, 2])
+        tree.delete(5)
+        assert(tree.search(5) == False)
+        remaining = [7, 2, 9, 3]
+        for o in remaining:
+            assert(tree.search(o))
+        for o in remaining:
+            tree.delete(o)
+        assert(tree.root is None)
+
+    def test_minmax(self):
+        tree = BST([5, 9, 7, 3, 2])
+        assert(tree.minimum == 2)
+        assert(tree.maximum == 9)
+        assert(BST().minimum is None)
+        assert(BST().maximum is None)
+
+    def test_depth(self):
+        assert(BST().depth == 0)
+        assert(BST([4]).depth == 1)
+        assert(BST([3, 2]).depth == 2)
+        assert(BST([5, 9, 7, 3, 2]).depth == 3)
+
+    def test_len(self):
+        assert(len(BST([5, 9, 7, 3, 2])) == 5)
+        assert(len(BST([3, 2])) == 2)
+
+    def test_equals(self):
+        tree = BST([5, 9, 7, 3, 2])
+        assert(tree == tree)
+        assert(tree == BST([5, 9, 7, 3, 2]))
+        assert(not(tree == BST([5, 9, 7, 3, 2, 8])))
+        assert(not(tree == BST([2, 3, 7, 9, 5])))
+        assert(not(tree == BST()))
+
+    def test_validate(self):
+        tree = BST([5, 9, 7, 3, 2])
+        assert(tree.validate())
+        tree.root.data = 10
+        assert(tree.validate() is False)
+        tree = BST(['g', 'b', 'd', 'h', 't', 'z', 'a'])
+        assert(tree.validate())
+
+    def test_traverse_in_order(self):
+        tree = BST([5, 9, 7, 3, 2])
+        assert(tree.traverse_in_order() == [2, 3, 5, 7, 9])
+        assert(BST().traverse_in_order() == [])
+
+    def test_traverse_level_order(self):
+        tree = BST([5, 9, 7, 3, 2])
+        expected = [[5], [3, 9], [2, None, 7, None]]
+        assert(tree.traverse_level_order() == expected)
+        assert(BST().traverse_level_order() == [])
+
+    def test_traverse_paths(self):
+        tree = BST([5, 9, 7, 3, 2])
+        expected = [[5, 3, 2], [5, 9, 7]]
+        assert(tree.traverse_paths() == expected)
+
+    def test_has_path_sum(self):
+        tree = BST([5, 9, 7, 3, 2])
+        assert(tree.has_path_sum(10))
+        assert(tree.has_path_sum(21))
+        assert(not tree.has_path_sum(17))
+        assert(BST().has_path_sum(0))
+        assert(BST([4]).has_path_sum(4))
 
 
 if __name__ == '__main__':
     from random import randint
+    from pprint import pprint
 
-    tree = BST()
-    for i in range(0, 10):
-        tree.insert(randint(0, 99))
-    # tree.root = Node(5)
-    # node = tree.root
-    # node.left = Node(2)
-    # node.right = Node(8)
-    # node = tree.root.left
-    # node.left = Node(1)
-    # node.right = Node(3)
-    # node = tree.root.right
-    # node.left = Node(7)
-    # node.right = Node(9)
-
-    # tree.bfs()
-    print(tree.levels())
-    print(tree)
+    tree = BST([5, 2, 8, 1, 3, 7, 9])
+    for i in range(0, 20):
+        tree.insert(randint(0, 100))
+    tree.validate()
+    print(tree.traverse_in_order())
+    pprint(tree.traverse_level_order())
+    pprint(tree.traverse_paths())
